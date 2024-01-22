a2abd309d1b3f24905558d0357e004b294ab6917
Konstantin Voykov <koykov0@gmail.com>
2024-01-22 22:36:03 +0200

    implement AVX2 index

---
+++ index_avx2.c
@@ -0,0 +1,128 @@
+#include <stdint.h>
+#include <string.h>
+#include <immintrin.h>
+#include "index_avx2.h"
+#include "cmp.h"
+
+inline size_t __attribute__((always_inline)) index_avx2_long(const char* s, size_t sz, const char* ss, size_t ssz) {
+    const __m256i first = _mm256_set1_epi8(ss[0]);
+    const __m256i last  = _mm256_set1_epi8(ss[ssz - 1]);
+
+    for (size_t i = 0; i < sz; i += 32) {
+        const __m256i block_first = _mm256_loadu_si256((const __m256i*)(s + i));
+        const __m256i block_last  = _mm256_loadu_si256((const __m256i*)(s + i + ssz - 1));
+
+        const __m256i eq_first = _mm256_cmpeq_epi8(first, block_first);
+        const __m256i eq_last  = _mm256_cmpeq_epi8(last, block_last);
+
+        uint32_t mask = _mm256_movemask_epi8(_mm256_and_si256(eq_first, eq_last));
+        while (mask != 0) {
+            int bitpos = __builtin_ctz(mask);
+            if (memcmp(s + i + bitpos + 1, ss + 1, ssz - 2) == 0) {
+                return i + bitpos;
+            }
+            mask = mask & (mask - 1);
+        }
+    }
+
+    return -1;
+}
+
+inline size_t __attribute__((always_inline)) index_2_avx2(const char* s, size_t sz, const char* ss, size_t ssz) {
+    __m256i broadcasted[ssz];
+    for (unsigned i=0; i < ssz; i++) {
+        broadcasted[i] = _mm256_set1_epi8(ss[i]);
+    }
+
+    __m256i curr = _mm256_loadu_si256((const __m256i*)(s));
+    for (size_t i = 0; i < sz; i += 32) {
+        const __m256i next = _mm256_loadu_si256((const __m256i*)(s + i + 32));
+        __m256i eq = _mm256_cmpeq_epi8(curr, broadcasted[0]);
+        __m256i next1;
+        next1 = _mm256_inserti128_si256(next1, _mm256_extracti128_si256(curr, 1), 0); // b
+        next1 = _mm256_inserti128_si256(next1, _mm256_extracti128_si256(next, 0), 1); // c
+        for (size_t j=1; j < ssz; j++) {
+            const __m256i substring = _mm256_alignr_epi8(next1, curr, (int)j);
+            eq = _mm256_and_si256(eq, _mm256_cmpeq_epi8(substring, broadcasted[j]));
+        }
+        curr = next;
+
+        const uint32_t mask = _mm256_movemask_epi8(eq);
+        if (mask != 0) {
+            return i + __builtin_ctzl(mask);
+        }
+    }
+
+    return -1;
+}
+
+inline size_t __attribute__((always_inline)) index_avx2_cmpfn(const char* s, size_t sz, const char* ss, size_t ssz, cmpfn fn) {
+    const __m256i first = _mm256_set1_epi8(ss[0]);
+    const __m256i last  = _mm256_set1_epi8(ss[ssz - 1]);
+
+    for (size_t i = 0; i < sz; i += 32) {
+        const __m256i block_first = _mm256_loadu_si256((const __m256i*)(s + i));
+        const __m256i block_last  = _mm256_loadu_si256((const __m256i*)(s + i + ssz - 1));
+
+        const __m256i eq_first = _mm256_cmpeq_epi8(first, block_first);
+        const __m256i eq_last  = _mm256_cmpeq_epi8(last, block_last);
+
+        uint32_t mask = _mm256_movemask_epi8(_mm256_and_si256(eq_first, eq_last));
+        while (mask != 0) {
+            int bitpos = __builtin_ctz(mask);
+            if (fn(s + i + bitpos + 1, ss + 1)) {
+                return i + bitpos;
+            }
+            mask = mask & (mask - 1);
+        }
+    }
+
+    return -1;
+}
+
+size_t index_avx2(const char *s, size_t sz, const char *ss, size_t ssz) {
+    if (sz < ssz) { return -1; }
+    size_t pos;
+    switch (ssz) {
+        case 0:
+            return 0;
+        case 1: {
+            const char *res = (const char *) (strchr(s, ss[0]));
+            return (res != NULL) ? (int)(res - s) : -1;
+        }
+        case 2:
+            pos = index_2_avx2(s, sz, ss, ssz);
+            break;
+        case 3:
+            pos = index_avx2_cmpfn(s, sz, ss, ssz, cmp1);
+            break;
+        case 4:
+            pos = index_avx2_cmpfn(s, sz, ss, ssz, cmp2);
+            break;
+        case 5:
+        case 6:
+            pos = index_avx2_cmpfn(s, sz, ss, ssz, cmp4);
+            break;
+        case 7:
+            pos = index_avx2_cmpfn(s, sz, ss, ssz, cmp5);
+            break;
+        case 8:
+            pos = index_avx2_cmpfn(s, sz, ss, ssz, cmp6);
+            break;
+        case 9:
+        case 10:
+            pos = index_avx2_cmpfn(s, sz, ss, ssz, cmp8);
+            break;
+        case 11:
+            pos = index_avx2_cmpfn(s, sz, ss, ssz, cmp9);
+            break;
+        case 12:
+            pos = index_avx2_cmpfn(s, sz, ss, ssz, cmp10);
+            break;
+        default:
+            pos = index_avx2_long(s, sz, ss, ssz);
+            break;
+    }
+    if (pos > sz-ssz) pos = -1;
+    return pos;
+}
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "index_sse2.h"
 #include "index_sse4.h"
+#include "index_avx2.h"
 
 int main() {
     const char *l = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed eros fringilla, porttitor erat sed, tempus quam. Vivamus vitae convallis sem, et rutrum arcu. Fusce eu vehicula diam. Nulla pulvinar fermentum lacus. Nulla orci leo, auctor nec nisl eget, aliquam consequat orci. Donec ultrices laoreet metus, eu auctor sapien sollicitudin eget. In ornare molestie ullamcorper. Donec ac purus nec leo placerat tincidunt. Phasellus sed enim odio. Nam erat nisl, placerat sit amet ultrices in, vulputate eu est. In vestibulum fringilla mauris, non tincidunt diam gravida vel. Proin est nisi, elementum sed risus vel, porttitor porttitor elit!";
@@ -9,14 +10,19 @@ int main() {
     const char *ss1 = "purus";
     const char *ss2 = "vestibulum";
     const char *ss3 = "vestibulum fringilla mauris";
-    printf("sse2: %d\n", index_sse2(l, strlen(l), ss, strlen(ss)));
-    printf("sse2: %d\n", index_sse2(l, strlen(l), ss1, strlen(ss1)));
-    printf("sse2: %d\n", index_sse2(l, strlen(l), ss2, strlen(ss2)));
-    printf("sse2: %d\n", index_sse2(l, strlen(l), ss3, strlen(ss3)));
+    printf("sse2: %zu\n", index_sse2(l, strlen(l), ss, strlen(ss)));
+    printf("sse2: %zu\n", index_sse2(l, strlen(l), ss1, strlen(ss1)));
+    printf("sse2: %zu\n", index_sse2(l, strlen(l), ss2, strlen(ss2)));
+    printf("sse2: %zu\n", index_sse2(l, strlen(l), ss3, strlen(ss3)));
 
-    printf("sse4: %d\n", index_sse4(l, strlen(l), ss, strlen(ss)));
-    printf("sse4: %d\n", index_sse4(l, strlen(l), ss1, strlen(ss1)));
-    printf("sse4: %d\n", index_sse4(l, strlen(l), ss2, strlen(ss2)));
-    printf("sse4: %d\n", index_sse4(l, strlen(l), ss3, strlen(ss3)));
+    printf("sse4: %zu\n", index_sse4(l, strlen(l), ss, strlen(ss)));
+    printf("sse4: %zu\n", index_sse4(l, strlen(l), ss1, strlen(ss1)));
+    printf("sse4: %zu\n", index_sse4(l, strlen(l), ss2, strlen(ss2)));
+    printf("sse4: %zu\n", index_sse4(l, strlen(l), ss3, strlen(ss3)));
+
+    printf("avx2: %zu\n", index_avx2(l, strlen(l), ss, strlen(ss)));
+    printf("avx2: %zu\n", index_avx2(l, strlen(l), ss1, strlen(ss1)));
+    printf("avx2: %zu\n", index_avx2(l, strlen(l), ss2, strlen(ss2)));
+    printf("avx2: %zu\n", index_avx2(l, strlen(l), ss3, strlen(ss3)));
     return 0;
 }
